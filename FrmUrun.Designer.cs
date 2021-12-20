2da2474246ed30ac080711240496a1163982eff5
Marzooq Moftah <83363867+MaMo77570@users.noreply.github.com>
2021-12-20 16:18:15 +0300

    Database Project
    This is my project for Database

---
+++ FrmUrun.Designer.cs
@@ -0,0 +1,335 @@
+﻿
+namespace PostgresUrunProje
+{
+    partial class FrmUrun
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtAd = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TxtAlisFiyat = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.TxtSatisFiyat = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.TxtGorsel = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.BtnAra = new System.Windows.Forms.Button();
+            this.BtnGuncelle = new System.Windows.Forms.Button();
+            this.BtnSil = new System.Windows.Forms.Button();
+            this.BtnEkle = new System.Windows.Forms.Button();
+            this.BtnListele = new System.Windows.Forms.Button();
+            this.BtnView = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TxtAd
+            // 
+            this.TxtAd.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TxtAd.Location = new System.Drawing.Point(318, 18);
+            this.TxtAd.Name = "TxtAd";
+            this.TxtAd.Size = new System.Drawing.Size(145, 20);
+            this.TxtAd.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(245, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Urun Adı :";
+            // 
+            // TxtID
+            // 
+            this.TxtID.Location = new System.Drawing.Point(79, 18);
+            this.TxtID.Name = "TxtID";
+            this.TxtID.Size = new System.Drawing.Size(145, 20);
+            this.TxtID.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(11, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Urun ID :";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.Brown;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(15, 142);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(949, 221);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(499, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Stok";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(80, 82);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Alış Fiyat";
+            // 
+            // TxtAlisFiyat
+            // 
+            this.TxtAlisFiyat.Location = new System.Drawing.Point(144, 78);
+            this.TxtAlisFiyat.Name = "TxtAlisFiyat";
+            this.TxtAlisFiyat.Size = new System.Drawing.Size(145, 20);
+            this.TxtAlisFiyat.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(311, 82);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Satış Fiyat";
+            // 
+            // TxtSatisFiyat
+            // 
+            this.TxtSatisFiyat.Location = new System.Drawing.Point(383, 78);
+            this.TxtSatisFiyat.Name = "TxtSatisFiyat";
+            this.TxtSatisFiyat.Size = new System.Drawing.Size(145, 20);
+            this.TxtSatisFiyat.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(554, 82);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Görsel";
+            // 
+            // TxtGorsel
+            // 
+            this.TxtGorsel.Location = new System.Drawing.Point(606, 78);
+            this.TxtGorsel.Name = "TxtGorsel";
+            this.TxtGorsel.Size = new System.Drawing.Size(145, 20);
+            this.TxtGorsel.TabIndex = 7;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(747, 22);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(54, 13);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Kategori";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDown1.Location = new System.Drawing.Point(541, 18);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(145, 20);
+            this.numericUpDown1.TabIndex = 14;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(807, 18);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(145, 21);
+            this.comboBox1.TabIndex = 15;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // BtnAra
+            // 
+            this.BtnAra.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.BtnAra.BackColor = System.Drawing.Color.Coral;
+            this.BtnAra.ForeColor = System.Drawing.Color.Black;
+            this.BtnAra.Location = new System.Drawing.Point(493, 385);
+            this.BtnAra.Name = "BtnAra";
+            this.BtnAra.Size = new System.Drawing.Size(145, 33);
+            this.BtnAra.TabIndex = 16;
+            this.BtnAra.Text = "Ara";
+            this.BtnAra.UseVisualStyleBackColor = false;
+            this.BtnAra.Click += new System.EventHandler(this.BtnAra_Click);
+            // 
+            // BtnGuncelle
+            // 
+            this.BtnGuncelle.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.BtnGuncelle.BackColor = System.Drawing.Color.Coral;
+            this.BtnGuncelle.ForeColor = System.Drawing.Color.Black;
+            this.BtnGuncelle.Location = new System.Drawing.Point(657, 385);
+            this.BtnGuncelle.Name = "BtnGuncelle";
+            this.BtnGuncelle.Size = new System.Drawing.Size(145, 33);
+            this.BtnGuncelle.TabIndex = 17;
+            this.BtnGuncelle.Text = "Guncelle";
+            this.BtnGuncelle.UseVisualStyleBackColor = false;
+            this.BtnGuncelle.Click += new System.EventHandler(this.BtnGuncelle_Click);
+            // 
+            // BtnSil
+            // 
+            this.BtnSil.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.BtnSil.BackColor = System.Drawing.Color.Coral;
+            this.BtnSil.ForeColor = System.Drawing.Color.Black;
+            this.BtnSil.Location = new System.Drawing.Point(330, 385);
+            this.BtnSil.Name = "BtnSil";
+            this.BtnSil.Size = new System.Drawing.Size(145, 35);
+            this.BtnSil.TabIndex = 18;
+            this.BtnSil.Text = "Sil";
+            this.BtnSil.UseVisualStyleBackColor = false;
+            this.BtnSil.Click += new System.EventHandler(this.BtnSil_Click);
+            // 
+            // BtnEkle
+            // 
+            this.BtnEkle.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.BtnEkle.BackColor = System.Drawing.Color.Coral;
+            this.BtnEkle.ForeColor = System.Drawing.Color.Black;
+            this.BtnEkle.Location = new System.Drawing.Point(819, 385);
+            this.BtnEkle.Name = "BtnEkle";
+            this.BtnEkle.Size = new System.Drawing.Size(145, 33);
+            this.BtnEkle.TabIndex = 19;
+            this.BtnEkle.Text = "Ekle";
+            this.BtnEkle.UseVisualStyleBackColor = false;
+            this.BtnEkle.Click += new System.EventHandler(this.BtnEkle_Click);
+            // 
+            // BtnListele
+            // 
+            this.BtnListele.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.BtnListele.BackColor = System.Drawing.Color.Coral;
+            this.BtnListele.ForeColor = System.Drawing.Color.Black;
+            this.BtnListele.Location = new System.Drawing.Point(164, 385);
+            this.BtnListele.Name = "BtnListele";
+            this.BtnListele.Size = new System.Drawing.Size(145, 36);
+            this.BtnListele.TabIndex = 20;
+            this.BtnListele.Text = "  Listele";
+            this.BtnListele.UseVisualStyleBackColor = false;
+            this.BtnListele.Click += new System.EventHandler(this.BtnListele_Click);
+            // 
+            // BtnView
+            // 
+            this.BtnView.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.BtnView.BackColor = System.Drawing.Color.Coral;
+            this.BtnView.ForeColor = System.Drawing.Color.Black;
+            this.BtnView.Location = new System.Drawing.Point(13, 385);
+            this.BtnView.Name = "BtnView";
+            this.BtnView.Size = new System.Drawing.Size(145, 33);
+            this.BtnView.TabIndex = 21;
+            this.BtnView.Text = "View";
+            this.BtnView.UseVisualStyleBackColor = false;
+            this.BtnView.Click += new System.EventHandler(this.BtnView_Click);
+            // 
+            // FrmUrun
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(975, 451);
+            this.Controls.Add(this.BtnView);
+            this.Controls.Add(this.BtnAra);
+            this.Controls.Add(this.BtnGuncelle);
+            this.Controls.Add(this.BtnSil);
+            this.Controls.Add(this.BtnEkle);
+            this.Controls.Add(this.BtnListele);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.TxtGorsel);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.TxtSatisFiyat);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.TxtAlisFiyat);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TxtAd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtID);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "FrmUrun";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.FrmUrun_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox TxtAd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TxtAlisFiyat;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox TxtSatisFiyat;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox TxtGorsel;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button BtnAra;
+        private System.Windows.Forms.Button BtnGuncelle;
+        private System.Windows.Forms.Button BtnSil;
+        private System.Windows.Forms.Button BtnEkle;
+        private System.Windows.Forms.Button BtnListele;
+        private System.Windows.Forms.Button BtnView;
+    }
+}
---
+++ FrmUrun.cs
@@ -0,0 +1,132 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PostgresUrunProje
+{
+    public partial class FrmUrun : Form
+    {
+        public FrmUrun()
+        {
+            InitializeComponent();
+        }
+
+        private NpgsqlConnection start = new NpgsqlConnection("server=localhost;" +
+                " port=7757;" +
+                "DataBase=dburunler;" +
+                "user ID=postgres;" +
+                "password=775705161");
+
+        // show the database on the screen
+        private void BtnListele_Click(object sender, EventArgs e)
+        {
+            string sorgu = "select * from urunler";
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter(sorgu, start);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        // start form the begining
+        private void FrmUrun_Load(object sender, EventArgs e)
+        {
+            start.Open();
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter("Select * from kategoriler", start);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            comboBox1.DisplayMember = "kategoriad";
+            comboBox1.ValueMember = "kategoriid";
+            comboBox1.DataSource = dt;
+            start.Close();
+        }
+
+        // this is the function where we can add a new items
+        private void BtnEkle_Click(object sender, EventArgs e)
+        {
+            start.Open();
+            NpgsqlCommand prosses1 = new NpgsqlCommand("insert into urunler " +
+                "(urunid,urunad,stok,alisfiyat,satisfiyat,gorsel,kategori) " +
+                "values(@p1,@p2, @p3, @p4, @p5,@p6, @p7)", start);
+
+            prosses1.Parameters.AddWithValue("@p1", int.Parse(TxtID.Text));
+            prosses1.Parameters.AddWithValue("@p2", TxtAd.Text);
+            prosses1.Parameters.AddWithValue("@p3", int.Parse(numericUpDown1.Value.ToString()));
+            prosses1.Parameters.AddWithValue("@p4", double.Parse(TxtAlisFiyat.Text));
+            prosses1.Parameters.AddWithValue("@p5", double.Parse(TxtSatisFiyat.Text));
+            prosses1.Parameters.AddWithValue("@p6", TxtGorsel.Text);
+            prosses1.Parameters.AddWithValue("@p7", int.Parse(comboBox1.SelectedValue.ToString()));
+
+            prosses1.ExecuteNonQuery();
+            start.Close();
+            MessageBox.Show("kayid basarli oldu :) ", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        // We can update any informartion by using this function
+        private void BtnGuncelle_Click(object sender, EventArgs e)
+        {
+            start.Open();
+            NpgsqlCommand prosses2 = new NpgsqlCommand("update urunler set urunad=@p1,stok=@p2," +
+                "alisfiyat=@p3 where urunid=@p4", start);
+            prosses2.Parameters.AddWithValue("@p1", TxtAd.Text);
+            prosses2.Parameters.AddWithValue("@p2", int.Parse(numericUpDown1.Value.ToString()));
+            prosses2.Parameters.AddWithValue("@p3", double.Parse(TxtAlisFiyat.Text));
+            prosses2.Parameters.AddWithValue("@p4", int.Parse(TxtID.Text));
+            prosses2.ExecuteNonQuery();
+            MessageBox.Show("Basrili Gucelleme :) ", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            start.Close();
+            start.Close();
+            start.Close();
+            start.Close();
+            start.Close();
+            start.Close();
+        }
+
+        // if we want to show the database on the screen we can use this function
+        private void BtnView_Click(object sender, EventArgs e)
+        {
+            start.Open();
+            NpgsqlCommand prosses3 = new NpgsqlCommand("Select * from urunlistesi", start);
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter(prosses3);
+            DataSet dt = new DataSet();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt.Tables[0];
+            start.Close();
+        }
+
+        // of course we must have delete function to delete any information
+        private void BtnSil_Click(object sender, EventArgs e)
+        {
+            start.Open();
+            NpgsqlCommand prosses3 = new NpgsqlCommand("DElete From urunler where urunid=@p1",
+                start);
+            prosses3.Parameters.AddWithValue("@p1", int.Parse(TxtID.Text));
+            prosses3.ExecuteNonQuery();
+            start.Close();
+            MessageBox.Show("Silme basirli oldu", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+        }
+
+        // if we have a lot of information we need the function search,to find any data fastly.
+        private void BtnAra_Click(object sender, EventArgs e)
+        {
+            start.Open();
+            string sql = "Select * from kategoriler where kategoriid=" + TxtID.Text;
+            NpgsqlCommand cmd = new NpgsqlCommand(sql, start);
+            DataTable dt = new DataTable();
+            dt.Load(cmd.ExecuteReader());
+            dataGridView1.DataSource = dt;
+            start.Close();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
